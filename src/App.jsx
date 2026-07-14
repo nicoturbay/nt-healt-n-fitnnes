76cfb72bafcc4bfe0fa494ae58405656ce873957
Nicolas Turbay <nturbay@gmail.com>
2026-07-14 11:21:41 -0400

    update: src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
-import Today from './pages/Today'
+import Dashboard from './pages/Dashboard'
+import Workout from './pages/Workout'
 import Log from './pages/Log'
 import Nutrition from './pages/Nutrition'
 import Progress from './pages/Progress'
-import Photos from './pages/Photos'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Today />} />
-        <Route path="log" element={<Log />} />
+        <Route index element={<Dashboard />} />
+        <Route path="workout" element={<Workout />} />
         <Route path="nutrition" element={<Nutrition />} />
         <Route path="progress" element={<Progress />} />
-        <Route path="photos" element={<Photos />} />
+        <Route path="log" element={<Log />} />
       </Route>
     </Routes>
   )
